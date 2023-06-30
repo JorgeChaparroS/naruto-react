4f51c5a6eecb6806de0567ab200a5e0991dc6502
JorgeChaparroS <jorgechaparrosarmiento@gmail.com>
2023-06-29 20:52:40 -0500

    Authors page

--- pages/characters/index.js
+++ pages/characters/index.js
@@ -1,23 +1,23 @@
 import Head from 'next/head';
 import Link from 'next/link';
-import Layout from '../components/layout/layout';
+import Layout from '../../components/layout/layout';
 import { useRouter } from 'next/router';
-import en from '../public/i18n/en';
-import es from '../public/i18n/es';
+import en from '../../public/i18n/en';
+import es from '../../public/i18n/es';
 
-export default function Author() {
+export default function Characters() {
 
     const router = useRouter();
     const { locale } = router;
     const i18n = locale === 'es' ? es : en;
-    
+
     return (
         <Layout>
             <Head>
-                <title>{`${i18n.header} - Jorge Chaparro`}</title>
+                <title>{`${i18n.header} - ${i18n.mainPage.charactersCard.title}`}</title>
             </Head>
             <main className='container mx-auto content-page-container'>
-                <h1>Jorge Chaparro</h1>
+                <h1>Personajes</h1>
                 <Link href="/">Volver</Link>
             </main>
         </Layout>
